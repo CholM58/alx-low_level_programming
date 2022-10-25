5c6423b7d807bf4bf41153d45bce8558ccacfe1d
CholM58 <magolaliet58@gmail.com>
2022-10-25 03:34:12 -0700

    pointers_and_arrays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,11 @@
 
 /**
  * void reset_t0_98 - a function that takes a pointer to an int as paramete
- * @n: A pointer to an integer
- * Return: Always 0 ( success)
+ * @n: pointer to an integer
+ * Return: 0 (success)
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 *n = 98;
 }
